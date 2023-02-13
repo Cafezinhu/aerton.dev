a84b4be433e53d5db275a24ec2b2986d1616a00f
Aerton <aertoncastro@gmail.com>
2023-02-13 12:28:22 -0300

    contatos

---
+++ src/components/ContatoIcon.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+    export let href: string;
+    export let text: string;
+
+    function click(e: MouseEvent){
+        if(href == ""){
+            e.preventDefault();
+            navigator.clipboard.writeText(text);
+        }
+    }
+</script>
+
+<a href={href}
+class="flex icon transition-all items-center gap-2"
+on:click={click}>
+    <div class="flex w-10">
+        <slot />
+    </div>
+    <p class="text-primary-1">{text}</p>
+</a>
+
+<style lang="postcss">
+    .icon{
+        filter: invert(37%) sepia(6%) saturate(2842%) hue-rotate(213deg) brightness(91%) contrast(83%);
+    }
+
+    .icon:hover{
+        filter: invert(77%) sepia(52%) saturate(550%) hue-rotate(70deg) brightness(98%) contrast(105%);
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,41 @@
+<script lang="ts">
+    import FaLinkedin from 'svelte-icons/fa/FaLinkedin.svelte';
+    import FaGithubSquare from 'svelte-icons/fa/FaGithubSquare.svelte';
+    import FaDiscord from 'svelte-icons/fa/FaDiscord.svelte';
+    import ContatoIcon from '../components/ContatoIcon.svelte';
+
+    let contatos: HTMLDivElement;
+
+    function scrollToContatos(){
+        contatos.scrollIntoView({behavior: 'smooth'});
+    }
+</script>
+
 <main class="bg-primary-4 h-screen flex justify-center items-center">
-    <div>
+    <div class="p-4">
         <p class="text-secondary-1 text-xl">Olá, meu nome é</p>
-        <h1 class="text-gray-200 text-9xl mb-2">Aerton Oliveira</h1>
+        <h1 class="text-gray-200 text-5xl md:text-6xl lg:text-9xl mb-2">Aerton Oliveira</h1>
         <h2 class="text-gray-300 text-xl">Sou desenvolvedor de software, construtor de sites, aplicativos e games.</h2>
-        <button class="border-secondary-1 rounded-md border-2 text-secondary-1 hover:bg-secondary-1 hover:text-primary-4 transition-all text-xl p-1 mt-3">Entrar em contato</button>
+        <button 
+            class="border-secondary-1 rounded-md border-2 text-secondary-1 hover:bg-secondary-1 hover:text-primary-4 transition-all text-xl p-1 mt-3"
+            on:click={scrollToContatos}>
+            Entrar em contato
+        </button>
         <a class="border-tertiary-1 rounded-md border-2 text-tertiary-1 hover:bg-tertiary-1 hover:text-primary-4 transition-all text-xl p-1 ml-2 cursor-pointer" href="/blog">Blog</a>
     </div>
 </main>
+
+<div class="bg-primary-4 flex flex-col justify-center items-center" bind:this={contatos}>
+    <h1 class="text-gray-200 text-4xl">Contato</h1>
+    <div class="flex flex-col my-4 gap-4">
+        <ContatoIcon href="https://www.linkedin.com/in/aerton-castro-de-oliveira/" text="in/aerton-castro-de-oliveira">
+            <FaLinkedin />
+        </ContatoIcon>
+        <ContatoIcon href="https://github.com/Cafezinhu" text="Cafezinhu">
+            <FaGithubSquare />
+        </ContatoIcon>
+        <ContatoIcon href="" text="Cafezinho#0059">
+            <FaDiscord />
+        </ContatoIcon>
+    </div>
+</div>
